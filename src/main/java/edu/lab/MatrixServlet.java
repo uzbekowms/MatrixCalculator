7303fbdd3d1c44e9e3d4bd31c51249b1f45f5b0c
uzbek <lukasowms@gmail.com>
2023-12-11 00:29:08 +0200

    Migrate launch to maven

--- src/main/java/edu/lab/MatrixServlet.java
+++ src/main/java/edu/lab/MatrixServlet.java
@@ -1,13 +1,12 @@
 package edu.lab;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
